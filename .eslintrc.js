c43a29459747b79481c5b1e1a528ad2792e9d792
divyansh <divyanshgoel8860@gmail.com>
2025-09-29 13:28:32 +0530

    removed user docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'next'],
   rules: {
     '@next/next/no-img-element': 'off',
     'jsx-a11y/alt-text': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/no-unescaped-entities': 'off',
   },
 };
